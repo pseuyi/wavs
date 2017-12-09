ae580dd3ba104764c12e1fac67a6d98481d8cd21
pseuyi <pseuyi@gmail.com>
2017-12-09 16:30:09 -0500

    update title text

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,19 +3,42 @@ import {Entity} from 'aframe-react';
 
 const Title = () => (
   <Entity>
-    <Entity text={{value: 'w/a/v/s/w/a/v/s', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
-    <Entity position={{x:0, y:1, z: -1}} text-geometry={{
-      value: 'underwater wa wa wa',
-      style: 'italic',
-      size: 0.8,
-      weight: 'bold',
-      height: 0,
-      bevelEnabled: true,
-      bevelSize: 0.04,
-      bevelThickness: 0.04,
-      curveSegments: 1,
-    }}
-      material={{ shader: 'flat', color: 'purple' }} />
+    <Entity
+      geometry={{primitive: 'plane'}}
+      position={{x:0, y:8, z: 0}}
+      text={{
+        value: '~ ~',
+        font: 'https://cdn.aframe.io/fonts/Roboto-msdf.json',
+        align: 'center',
+        color: 'green',
+        width: 44,
+      }}
+      material={{transparent: true, opacity: 0}}
+    />
+    <Entity
+      geometry={{primitive: 'plane'}}
+      position={{x:0, y:6, z: 0}}
+      text={{
+        value: 'wavs',
+        font: 'https://cdn.aframe.io/fonts/Roboto-msdf.json',
+        align: 'center',
+        color: 'blue',
+        width: 44,
+      }}
+      material={{transparent: true, opacity: 0}}
+    />
+    <Entity
+      geometry={{primitive: 'plane'}}
+      position={{x:0, y:4, z: 0}}
+      text={{
+        value: '~',
+        font: 'https://cdn.aframe.io/fonts/Roboto-msdf.json',
+        align: 'center',
+        color: 'red',
+        width: 44,
+      }}
+      material={{transparent: true, opacity: 0}}
+    />
   </Entity>
 )
 
