24606cce1c726c2f9033022a16c2049e0518c88d
pseuyi <pseuyi@gmail.com>
2017-12-09 16:29:48 -0500

    remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import 'aframe';
-import 'aframe-text-geometry-component';
 import 'aframe-animation-component';
 import 'aframe-particle-system-component';
 import 'babel-polyfill';
-import {Entity, Scene} from 'aframe-react';
+import {Scene} from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
